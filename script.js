23b11dd8ce0d3a37efcf29ec0bd4cb24e4819e7e
Tom <thomascatchlove@gmail.com>
2020-09-25 14:55:27 +1000

    footer trigger by scroll event

--- script.js
+++ script.js
@@ -2,42 +2,47 @@
 
 $(document).ready(function(){
 
-    $("a").on('click', function(event) {
-  
-      if (this.hash !== "") {
-        event.preventDefault();
-  
-        var hash = this.hash;
-  
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          window.location.hash = hash;
-        });
-      } 
-    });
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+    
+        window.location.hash = hash;
+      });
+    } 
   });
+});
   
-  VANTA.WAVES({
-    el: "#ocean",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: '#5271ff',
-    shininess: 150.00,
-    waveHeight: 40.00,
-    waveSpeed: 2.00,
-    zoom: 1.49
-  })
+VANTA.WAVES({
+  el: "#ocean",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: '#5271ff',
+  shininess: 150.00,
+  waveHeight: 40.00,
+  waveSpeed: 2.00,
+  zoom: 1.49
+})
   
-  const footer = document.querySelector('footer');
-  const scrollClick = document.querySelector('.scroll_click');
-  
-  scrollClick.addEventListener('click', () => {
-    footer.classList.remove('invisible');
-  })
+const footer = document.querySelector('footer');
+
+$(window).scroll(function() {
+  var hT = $('#scroll-to').offset().top,
+      hH = $('#scroll-to').outerHeight(),
+      wH = $(window).height(),
+      wS = $(this).scrollTop();
+  if (wS > (hT+hH-wH)){
+    footer.classList.remove('invisible');    
+  }
+});
